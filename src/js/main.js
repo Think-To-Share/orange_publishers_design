8aea433fae77773d7119deb11b3b96ae220fc12b
Rahul Dey <rahul900day@gmail.com>
2019-09-11 12:11:35 +0530

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// All Stylesheets
+// import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/@fortawesome/fontawesome-free/css/fontawesome.css';
+import '../../node_modules/@fortawesome/fontawesome-free/css/brands.css';
+import '../../node_modules/@fortawesome/fontawesome-free/css/solid.css';
+import '../scss/bootstrap.scss';
+import '../scss/app.scss';
+
+// All Javascript
+window.jquery = window.$ = require('jquery');
+
+// require('select2');
+require('./homepage');
