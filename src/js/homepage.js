dd41ba847ffd54f44065a4f45f735a94f62d2508
Rahul Dey <rahul900day@gmail.com>
2019-10-23 14:52:28 +0530

    changes made

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -28,27 +28,16 @@ $(window).on('load', function() {
     let aboutSectionVideo = $('.orange-about-section .orange-about-video .video-main video');
     let aboutSectionVideoPlay = $('.orange-about-section .container .orange-about-video .video-main .play-icon');
     let aboutSectionVideoOverlay = $('.orange-about-section .container .orange-about-video .video-main .overlay');
-    let aboutSectionVideoPaused = false;
 
     aboutSectionVideoPlay.click(() => {
         aboutSectionVideoOverlay.fadeOut();
         aboutSectionVideoPlay.fadeOut();
+        aboutSectionVideo.attr('controls', true);
         aboutSectionVideo.get(0).play();
 
         aboutSectionVideo.on('ended', function() {
             aboutSectionVideoEnded = true;
         })
-    });
-
-    aboutSectionVideo.click(() => {
-        if(!aboutSectionVideoPaused) {
-            aboutSectionVideo.get(0).pause();
-            aboutSectionVideoPaused = true;
-            return;
-        }
-
-        aboutSectionVideo.get(0).play();
-        aboutSectionVideoPaused = false;
     });
 
     // Banner Query Form 
@@ -61,30 +50,38 @@ $(window).on('load', function() {
         $(this).siblings('.servicece-list').fadeOut('fast');
     });
 
+    UpdateBannerCat($('#bannerServiceCat'));
     $('#bannerServiceCat').change(function() {
-        let catId = $(this).children('option:selected').attr('value');
-        let url = `http://localhost/orange/wp-json/wp/v2/pages?service_cat=${catId}`;
-
-        $.ajax({
-            url: url
-        }).done(function(data) {
-            $('.servicece-list').children('li').remove();
-
-            if(data[0] != undefined) {
-                data.forEach(element => {
-                    $('.servicece-list').append(`<li><a href="${element.link}">${element.title.rendered}</a></li>`)
-                });
-            }
-        })
+        UpdateBannerCat($(this));
     });
 
     // Book Cover Section
     $('.new-book-cat-title a').click(function(event) {
         event.preventDefault();
         let bookCatId = $(this).data("book");
-        $(this)
+
+        $('.new-book-cat-title').removeClass('active');
+        $(this).parents('.new-book-cat-title').addClass('active');
+
         $('.orange-new-book-cat-content .books').hide();
         $(bookCatId).css('display', 'flex');
     });
 
 });
+
+function UpdateBannerCat(thisElem) {
+    let catId = thisElem.children('option:selected').attr('value');
+    let url = `http://localhost/orange/wp-json/wp/v2/pages?service_cat=${catId}`;
+
+    $.ajax({
+        url: url
+    }).done(function(data) {
+        $('.servicece-list').children('li').remove();
+
+        if(data[0] != undefined) {
+            data.forEach(element => {
+                $('.servicece-list').append(`<li><a href="${element.link}">${element.title.rendered}</a></li>`)
+            });
+        }
+    })
+}
